0d8b041bac4b3048b4203ffdcb19f8973ea5997d
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2022-01-16 17:08:15 +0800

    create billing_addresses table

---
+++ src/Payment/Infrastructure/Database/Migrations/2022_01_16_085919_create_billing_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billing_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->string('country');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('address_line_one');
+            $table->string('address_line_two')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billing_addresses');
+    }
+}
