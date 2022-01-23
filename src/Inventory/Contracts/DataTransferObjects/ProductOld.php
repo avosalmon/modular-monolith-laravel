8dc5137ca215b3a4a07902b581bb14c40ffd795f
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2022-01-23 15:43:11 +0800

    add Product DTO in old syntax

---
+++ src/Inventory/Contracts/DataTransferObjects/ProductOld.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laracon\Inventory\Contracts\DataTransferObjects;
+
+/**
+ * Product DTO implementation in older version of PHP.
+ */
+class ProductOld
+{
+    private int $id;
+
+    private string $name;
+
+    private int $price;
+
+    public function __construct(
+        int $id,
+        string $name,
+        int $price,
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+}
